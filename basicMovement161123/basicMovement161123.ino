b039e8c834dbe6677aeb6f99e0e252fd2e5b8902
max1zac <thezacmax@gmail.com>
2024-03-11 12:18:49 +0000

    Changed testing code to test a 90 degree turn at full battery power

--- basicMovement161123/basicMovement161123.ino
+++ basicMovement161123/basicMovement161123.ino
@@ -8,9 +8,9 @@ void setup() {
   delay(1000);                        // Delay to finish tone
   servoLeft.attach(13);               // Attach left signal to pin 13 
   servoRight.attach(12);              // Attach right signal to pin 12
+  /*
   servoLeft.writeMicroseconds(1700);
   servoRight.writeMicroseconds(1300);
-
   delay(1000);
   servoLeft.writeMicroseconds(2000);
   servoRight.writeMicroseconds(2000);
@@ -18,7 +18,13 @@ void setup() {
   servoLeft.writeMicroseconds(1300);
   servoRight.writeMicroseconds(1700);
   delay(1000);
-  
+  */
+  servoLeft.writeMicroseconds(1600);
+  servoRight.writeMicroseconds(1600);
+  delay(1000);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500);  
+  delay(1000);
   servoLeft.detach();                 // Stop sending servo signals
   servoRight.detach(); 
 
